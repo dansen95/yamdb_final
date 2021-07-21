622921855761dc94df3f3d4bb31d42a99584eaba
Sen <daniil.sen@accenture.com>
2021-07-21 22:05:00 +0300

    18

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'DEFAULT_PARAM')
 
 DEBUG = False
 
